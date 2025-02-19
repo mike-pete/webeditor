be9e4b390608984b035675ca173e4036feca68ad
Mike Peterson <mike@lemonshell.com>
2025-02-19 11:19:30 -0800

    move to new rendering component

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,13 +1,71 @@
-import Block from './Block'
+import { SandpackLayout, SandpackPreview, SandpackProvider } from '@codesandbox/sandpack-react'
+import bime from '@mike.pete/bime'
+import { useEffect } from 'react'
+import useGenerateHTML from '../hooks/useGenerateHTML'
+import { useLayout } from '../stores/useLayout'
 
-const Canvas: React.FC<{ maxWidth: string }> = ({ maxWidth }) => {
+function Canvas({ maxWidth }: { maxWidth: string }) {
+	const html = useGenerateHTML()
+	const setSelectedBlockID = useLayout((state) => state.setSelectedBlockID)
+
+	useEffect(() => {
+		const selectComponent = (id: string) => setSelectedBlockID(id)
+
+		const model = { selectComponent }
+		const listener = bime.listen(model, '*')
+
+		return listener.cleanup
+	}, [setSelectedBlockID])
+
 	return (
-		<div
-			className='bg-white resize overflow-auto rounded-md h-[500px] w-[500px] min-h-[500px] min-w-[320px] max-h-[90%]'
-			style={{ maxWidth }}
-		>
-			<Block id={'root'} />
+		<SandpackProvider
+			options={{
+				externalResources: ['https://cdn.tailwindcss.com'],
+			}}
+			template={'react'}
+			customSetup={{
+				dependencies: {
+					'@mike.pete/bime': 'latest',
+					clsx: 'latest',
+				},
+			}}
+			files={{
+				'App.js': `
+import bime from '@mike.pete/bime'
+import clsx from 'clsx';
+import { useEffect, useState } from 'react'
+
+export default function Page() {
+	const remote = bime.remote(window.parent, '*')
+	const [selectedComponent, setSelectedComponent] = useState('root')
+
+	return (
+		<div className='min-h-screen h-screen'>
+			${html}
 		</div>
+	)
+}
+				`,
+			}}
+			style={{ height: '100%' }}
+		>
+			<SandpackLayout style={{ border: 'none', height: '100%', backgroundColor: 'transparent' }}>
+				<div className='flex flex-col gap-4 justify-center items-center w-full h-full'>
+					<div className='overflow-hidden rounded resize h-[90%] w-10/12' style={{ maxWidth }}>
+						<SandpackPreview
+							style={{ height: '100%' }}
+							showOpenInCodeSandbox={false}
+							showRestartButton={false}
+							showRefreshButton={false}
+						/>
+					</div>
+				</div>
+				{/* 
+				<SandpackFileExplorer />
+				<SandpackCodeEditor /> 
+				*/}
+			</SandpackLayout>
+		</SandpackProvider>
 	)
 }
 
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,75 +1,6 @@
-import { SandpackLayout, SandpackPreview, SandpackProvider } from '@codesandbox/sandpack-react'
-import bime from '@mike.pete/bime'
 import { useCallback, useEffect, useState } from 'react'
-import useGenerateHTML from '../hooks/useGenerateHTML'
 import useWindowResizeListener from '../hooks/useWindowResizeListener'
-import { useLayout } from '../stores/useLayout'
 import Canvas from './Canvas'
-
-function Runner({ maxWidth }: { maxWidth: string }) {
-	const html = useGenerateHTML()
-	const setSelectedBlockID = useLayout((state) => state.setSelectedBlockID)
-
-	useEffect(() => {
-		const selectComponent = (id: string) => setSelectedBlockID(id)
-
-		const model = { selectComponent }
-		const listener = bime.listen(model, '*')
-
-		return listener.cleanup
-	}, [setSelectedBlockID])
-
-	return (
-		<SandpackProvider
-			options={{
-				externalResources: ['https://cdn.tailwindcss.com'],
-			}}
-			template={'react'}
-			customSetup={{
-				dependencies: {
-					'@mike.pete/bime': 'latest',
-					clsx: 'latest',
-				},
-			}}
-			files={{
-				'App.js': `
-import bime from '@mike.pete/bime'
-import clsx from 'clsx';
-import { useEffect, useState } from 'react'
-
-export default function Page() {
-	const remote = bime.remote(window.parent, '*')
-	const [selectedComponent, setSelectedComponent] = useState('root')
-
-	return (
-		<div className='min-h-screen h-screen'>
-			${html}
-		</div>
-	)
-}
-				`,
-			}}
-			style={{ height: '100%' }}
-		>
-			<SandpackLayout style={{ border: 'none', height: '100%', backgroundColor: 'transparent' }}>
-				<div className='flex flex-col gap-4 justify-center items-center w-full h-full'>
-					<div className='overflow-hidden rounded resize h-[90%] w-10/12' style={{ maxWidth }}>
-						<SandpackPreview
-							style={{ height: '100%' }}
-							showOpenInCodeSandbox={false}
-							showRestartButton={false}
-							showRefreshButton={false}
-						/>
-					</div>
-				</div>
-				{/* 
-				<SandpackFileExplorer />
-				<SandpackCodeEditor /> 
-				*/}
-			</SandpackLayout>
-		</SandpackProvider>
-	)
-}
 
 const Preview: React.FC = () => {
 	const [maxWidth, setMaxWidth] = useState('0px')
@@ -89,14 +20,8 @@ const Preview: React.FC = () => {
 	useWindowResizeListener(updateMaxWidth)
 
 	return (
-		<div className='flex-grow grid grid-columns-1 grid-rows-2' ref={setWrapper}>
-			<div className='h-full w-full flex justify-center items-center'>
-				<Canvas maxWidth={maxWidth} />
-			</div>
-
-			<div className='h-full w-full bg-neutral-700'>
-				<Runner maxWidth={maxWidth} />
-			</div>
+		<div className='flex-grow' ref={setWrapper}>
+			<Canvas maxWidth={maxWidth} />
 		</div>
 	)
 }
